e885aaf34dcf848071491c9705d010a66524abdc
Harper Jiang <harperjiang@msn.com>
2017-04-10 22:26:19 -0500

    wa analysis

--- hw1/wa_analysis.py
+++ hw1/wa_analysis.py
@@ -25,24 +25,14 @@ embed = store.load()[0]
 
 d = embed.shape[0]
 
-min_nm = np.finfo(np.float64).max()
-max_nm = 0
-max_w = ''
-min_w = ''
+norms = [0.0] * d
 
 for w in word_dict:
     i = word_dict[w]
-    v = embed[i,:]
-    nm = np.linalg(v)
-    if nm > max_nm:
-        max_nm = nm
-        max_w = w
-    if nm < min_nm:
-        min_nm = nm
-        min_w = w
+    v = embed[i, :]
+    norms[i] = (np.linalg.norm(v), w)
+    
+norms.sort(key=lambda x: x[0])
 
-print(max_w)
-print(max_nm)
-
-print(min_w)
-print(min_nm)
+print(norms[:10])
+print(norms[:-10])
