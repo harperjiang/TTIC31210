e18e5bc27331d09ca56f42d858c735a6bdbaf280
Harper Jiang <harperjiang@msn.com>
2017-05-25 13:09:29 -0500

    hw3 gibbs annealing

--- hw3/gibbs_run_annealing.py
+++ hw3/gibbs_run_annealing.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from dataset import UDDataSet
-from gibbs import Gibbs, BetaSchedule
+from gibbs import Gibbs, BetaSqSchedule
 from hmm import HMM
 
 train_ds = UDDataSet('data/en-ud-train.conllu')
@@ -21,13 +21,13 @@ def predict(iteration):
         gt_tag = np.array([word[1] for word in sentence.words])
         num_correct_tag += (predict_tag == gt_tag).sum()
 
-    #print("Tag accuracy: %.4f" % (num_correct_tag / num_total_tag))
+    # print("Tag accuracy: %.4f" % (num_correct_tag / num_total_tag))
     return num_correct_tag / num_total_tag
 
 
 k = [1, 2, 5, 10, 100, 500, 1000, 2000]
 
-gibbs.beta_schedule = BetaSchedule()
+gibbs.beta_schedule = BetaSqSchedule()
 
 for ite in k:
     print("%d & %.4f\\\\\\hline" % (ite, predict(ite)))
